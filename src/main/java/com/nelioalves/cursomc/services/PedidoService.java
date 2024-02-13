fb2196314b6fee233ae2641573fcb2924d222794
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-12 23:16:55 -0300

    MockEmailService com Logger. Padroes strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -35,6 +35,9 @@ public class PedidoService {
     @Autowired
     private ClienteService clienteService;
 
+    @Autowired
+    private EmailService emailService;
+
     public Pedido find(Integer id) {
         Pedido obj = repo.findById(id).orElse(null);
         if (obj == null) {
@@ -64,7 +67,7 @@ public class PedidoService {
             ip.setPedido(obj);
         }
         itemPedidoRepository.saveAll(obj.getItens());
-        //emailService.sendOrderConfirmationEmail(obj);
+        emailService.sendOrderConfirmationEmail(obj);
         return obj;
     }
 }
