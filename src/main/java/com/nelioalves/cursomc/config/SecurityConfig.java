f84dfff98e8b121ca42930159a005338c9f4e190
Giovandro Santos <vsm.giovandro@gmail.com>
2024-02-14 22:55:34 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,7 +24,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -43,13 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/produtos/**",
             "/categorias/**",
             "/estados/**",
-            "/clientes/**",
     };
 
-//    private static final String[] PUBLIC_MATCHERS_POST = {
-//            "/clientes/**",
-//            "/auth/forgot/**"
-//    };
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/clientes/**",
+            "/auth/forgot/**"
+    };
 
     @Override
     protected void configure(HttpSecurity http)throws Exception{
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-//                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
